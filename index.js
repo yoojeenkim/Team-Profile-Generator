289a1ae2c00f261c299499eb4c6fe8c69d630c7a
daniel <yoojeenkim@gmail.com>
2021-10-08 19:18:52 -0400

    added prompt for manager

--- index.js
+++ index.js
@@ -1,4 +1,30 @@
+const inquirer = require("inquirer");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+const generateHtml = require("./lib/generateHTML");
 
+inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "manager",
+            message: "Enter Team Manager's Name"
+        },
+        {
+            type: "input",
+            name: "managerID",
+            message: "Enter Team Manager's Employee ID"
+        },
+        {
+            type: "input",
+            name: "manageremail",
+            message: "Enter Team Manager's Email Address"
+        },
+        {
+            type: "input",
+            name: "manageroffice",
+            message: "Enter Team Manager's Office Number"
+        }
+    .then()
+    ])
