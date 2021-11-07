55a4f8732e7e36243bb5c8417dbf50eeda7aaded
daniel <yoojeenkim@gmail.com>
2021-11-07 17:45:28 -0500

    added css

--- src/generateHtml.js
+++ src/generateHtml.js
@@ -51,18 +51,18 @@ function generateHtml(data) {
     <meta charset="UTF-8"/>
     <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <link rel="stylesheet" href="../node_modules/bootstrap/dist/css/bootstrap.min.css"/>
-    <link rel="stylesheet" href="./styles.css"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="style.css" rel="stylesheet">
     <title>Team Profile Generator</title>
 </head>
 <body>
-    <header class="d-lg-flex justify-content-lg-center align-items-lg-center">
+    <div class="d-lg-flex justify-content-lg-center align-items-lg-center team-header">
         <h2>My Team</h2>
-    </header>
+    </div>
     <div class="container-fluid d-flex flex-row justify-content-center flex-wrap">
         ${cards.join('\n')}
     </div>
-    <script src="../node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+    
 </body>
 </html>`;
 }
