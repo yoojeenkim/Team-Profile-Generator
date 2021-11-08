59026337cbf0e92eb0a892662447203be48a9cb7
daniel <yoojeenkim@gmail.com>
2021-11-08 02:18:43 -0500

    fixed all tests

--- tests/Intern.test.js
+++ tests/Intern.test.js
@@ -3,14 +3,23 @@ const Intern = require("../lib/Intern.js");
 describe("Intern", () => {
     describe("constructor", () => {
         it("should call the name, id, and email and associate it to the employee class", () => {
-            const newIntern = new Intern('name', 'id', 'email');
+            const value = {
+                name: "intern",
+                id: "2",
+                email: "intern@gmail.com",
+                school: "Georgia Tech"
+            };
+
+            const newIntern = new Intern(value);
             const name = newIntern.name;
             const id = newIntern.id;
             const email = newIntern.email;
+            const school = newIntern.school;
 
-            expect(name).toEqual('name');
-            expect(id).toEqual('id');
-            expect(email).toEqual('email');
+            expect(name).toEqual('intern');
+            expect(id).toEqual('2');
+            expect(email).toEqual('intern@gmail.com');
+            expect(school).toEqual('Georgia Tech');
         });
     });
 });
--- tests/Manager.test.js
+++ tests/Manager.test.js
@@ -3,14 +3,23 @@ const Manager = require("../lib/Manager.js");
 describe("Manager", () => {
     describe("constructor", () => {
         it("should call the name, id, and email and associate it to the employee class", () => {
+            const value = {
+                name: "manager",
+                id: "1",
+                email: "manager@gmail.com",
+                officenumber: "101"
+            };
+
             const Manager = new Manager(value);
             const name = newManager.name;
             const id = newManager.id;
             const email = newManager.email;
+            const officenumber = newManager.officenumber;
 
-            expect(name).toEqual('name');
-            expect(id).toEqual('id');
-            expect(email).toEqual('email');
+            expect(name).toEqual('manager');
+            expect(id).toEqual('1');
+            expect(email).toEqual('manager@gmail.com');
+            expect(officenumber).toEqual('101');
         });
     });
 });
